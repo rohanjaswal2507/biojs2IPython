f600509a619a1e18205692eddc232fe4d9af61c3
rohanjaswal2507 <rohanjaswal2507@gmail.com>
2016-07-20 00:22:14 +0530

    copy build js bundle to biojs_extensions

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import os, sys
+from shutil import copyfile
 
 def npmInstall(package):
     # Install the npm package
@@ -7,24 +8,45 @@ def npmInstall(package):
 
     # Build the node module for browser
     cwd = os.getcwd()
-    print(cwd)
     try:
         os.chdir(os.path.join(cwd, os.path.join('node_modules', package)))
         print(os.getcwd())
         os.system('npm install')
-        buildForBrowser()
+        buildForBrowser(package, cwd)
     except OSError:
         print("could not find the " + package + " npm package. Please make sure that the package has been downloaded!")
     else :
         os.chdir(os.path.join("..", ".."))
 
 
-def buildForBrowser():
+def buildForBrowser(package, rootDir):
     print('Building for Browser')
     os.system('npm run build')
+    if os.path.exists(os.path.join(os.getcwd(), 'build')):
+        try:
+            ''' code to copy the build js bundle to nbextensions directory
+                so that script could be loaded while running the notebook
+            '''
+            src = os.path.join(os.getcwd(), 'build')
+            file_names = os.listdir(src)
+            dest = os.path.join(rootDir, 'biojs_extensions')
+            for name in file_names:
+                src_file = os.path.join(src, name)
+                dest_file = os.path.join(dest, name)
+                copyfile(src_file, dest_file)
+
+        except OSError:
+            print('There was some problem while copying '+ package +' module to nbextensions. Please make sure that Jupyter notebook is configured properly')
+        else:
+            print(package + ' built successfully!')
+
+
+def install_nbextension():
+    os.system('jupyter nbextension install biojs_extensions --user')
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         for i in range (1, len(sys.argv)):
             pkg = sys.argv[i]
             npmInstall(pkg)
+        install_nbextension()
